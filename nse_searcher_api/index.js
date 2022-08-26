25b327774fb901fe13d9f31f3ff538bf42d55c2c
abijith365 <abhijithelampal005@gmail.com>
2022-08-26 20:48:05 +0530

    db connection dbconfig .env config

--- nse_searcher_api/index.js
+++ nse_searcher_api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import morgan from "morgan"
+import connect from './config/db.js'
 
 const app = express()
 
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 dotenv.config({ path: './config/config.env' })
 
+// DB connection
+connect()
 
 // server
 app.listen(process.env.PORT, () => { console.log("server is running on port 8888") })
